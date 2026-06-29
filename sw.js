15392191191138dbe09b035e21b64aa374fc11d9
BooguelsBrew <chrisguigue@hotmail.fr>
2026-06-30 01:55:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='booguels-tap-os-v11';
-const ASSETS=['./','./index.html','./style.css?v=11','./app.js?v=11','./manifest.webmanifest','./beers.json'];
+const CACHE='booguels-tap-os-20';
+const ASSETS=['./','./index.html','./style.css?v=20','./app.js?v=20','./manifest.webmanifest','./beers.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
