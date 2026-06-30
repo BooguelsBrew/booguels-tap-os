d4b4e145a47a9718ae69a1d8286ff9a5b62233b7
BooguelsBrew <chrisguigue@hotmail.fr>
2026-06-30 21:10:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='booguels-final-pwa-2';const ASSETS=['./','./index.html','./style.css?v=final2','./app.js?v=final2','./manifest.webmanifest','./beers.json'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='bb-taplist-locked10';
+const ASSETS=['./','./index.html','./style.css?v=locked10','./app.js?v=locked10','./manifest.webmanifest','./beers.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
